11a30f001df79d329e4b059f62c2783dec5bd209
Syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2025-01-25 19:29:30 -0800

    backend'

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import authRoutes from "./routes/auth.js";  // auth.js ko import karo
 
-// // MongoDB connection string
-
-// // Function to connect to the MongoDB database
-// const connectDB = async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(mongodbUri, {
-//       dbName: "HackathonDatabase",  // Set your database name here
-//     });
-
-//     console.log(`\n🌿 MongoDB connected successfully to ${connectionInstance.connection.name}! 🍃\n`);
-
-//     mongoose.connection.on("error", console.error.bind(console, "Connection error:"));
-
-//     process.on("SIGINT", () => {
-//       mongoose.connection.close(() => {
-//         console.log("Mongoose connection closed due to application termination.");
-//         process.exit(0);
-//       });
-//     });
-//   } catch (error) {
-//     console.error("MONGODB connection FAILED: ", error);
-//     process.exit(1); // Exits the process with an error code
-//   }
-// };
-
-// // Initialize Express app
-// const app = express();
-
-// // Middleware to parse JSON requests
-// app.use(express.json());
-
-// // Setup routes
-// app.use("/auth", authRoutes); // auth.js ke routes ko "/auth" prefix ke sath use karo
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Start the server
-// const PORT = 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 
 
 import express from "express";
@@ -82,7 +37,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:3000",  // Allow frontend (localhost:5173)
+  origin: "http://localhost:4000",  // Allow frontend (localhost:5173)
   methods: ["GET", "POST", "PUT", "DELETE"],  // Allow required HTTP methods
   credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
 };
